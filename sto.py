d1cbdbb87e64db62fd6e406ab05ea52f39964b7e
Đorđe Gluvajić <gluvajic@mail.com>
2018-03-04 13:56:20 +0100

    Karte se sada prikazuju bliže jedni drugima ako je njihov broj veći od 5

--- sto.py
+++ sto.py
@@ -25,21 +25,27 @@ class Sto:
         for x in range(0, 5):    
             prikaz.append("")
             for karta in self.karte:
+
+                prikaz[x] += "\t"
+
                 if x == 0 or x == 4:
-                    prikaz[x] += "\t-------\t"
+                    prikaz[x] += "-------"
                 elif x == 1:
                     if len(str(karta.charIme)) == 2:
-                        prikaz[x] += "\t|{0}{1}  |\t".format(karta.charIme, karta.charZnak)
+                        prikaz[x] += "|{0}{1}  |".format(karta.charIme, karta.charZnak)
                     else:
-                        prikaz[x] += "\t|{0}{1}   |\t".format(karta.charIme, karta.charZnak)
+                        prikaz[x] += "|{0}{1}   |".format(karta.charIme, karta.charZnak)
                 elif x == 2:
-                    prikaz[x] += "\t|     |\t"
+                        prikaz[x] += "|     |"
                 elif x == 3:
                     if len(str(karta.charIme)) == 2:
-                        prikaz[x] += "\t|  {0}{1}|\t".format(karta.charIme, karta.charZnak)
+                        prikaz[x] += "|  {0}{1}|".format(karta.charIme, karta.charZnak)
                     else:
-                        prikaz[x] += "\t|   {0}{1}|\t".format(karta.charIme, karta.charZnak)
+                        prikaz[x] += "|   {0}{1}|".format(karta.charIme, karta.charZnak)
+
+                if len(self.karte) <= 5:
+                    prikaz[x] += "\t"
 
         prikaz_formiran = "\n".join(prikaz)
-
         print("\n", prikaz_formiran) 
+            
